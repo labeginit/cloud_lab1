9390669029c2c16053ecf44394c4feee47293840
liliia <liliyameister@gmail.com>
2022-03-30 11:14:06 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//const port = process.env.PORT || 3001;
+const port = 3000;
+//for webserver to be created
+const express = require('express');
+//create a webserver using express
+const app = express();
+//serve all the files in the frontend folder
+app.use(express.static('frontend'));
+
+//Express.json is needed to read a request body (for POST/PUT/PATCH-request)
+app.use(express.json({limit: '100MB'}));
+
+//start the webserver on port 3000
+app.listen(port, () => console.log('//http:localhost:' + port));
+
+//import rest api functionality
+const setupRESTapi = require('./rest-api');
+setupRESTapi(app);
