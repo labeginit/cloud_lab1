a51db2ab61c343f17a272b6db1a27ad611507efa
liliia <liliyameister@gmail.com>
2022-03-31 08:37:08 +0200

    corrected port to work on heroku

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+const port = process.env.PORT || 3001;
 //for webserver to be created
 const express = require('express');
 //create a webserver using express
 const app = express();
 //serve all the files in the frontend folder
-app.listen(5001); 
+
+const path = require('path');
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('views'));
+
+//start the webserver on port 3000
+app.listen(port);
 
 //import rest api functionality
 const setupRESTapi = require('./rest-api');
